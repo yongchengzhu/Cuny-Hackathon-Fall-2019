53eda0517cc06f59f9ba8dc5fcb80d3ba5cbb03d
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2019-10-19 15:13:49 -0400

    Implemented both visuals.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import { default as React } from "react";
 import './App.css';
+import Bargraph from './components/bargraph.js';
+import Piechart from './components/piechart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bargraph
+        data={[{index: 0, date: 0, value: 15},
+               {index: 1, date: 1, value: 45}, 
+               {index: 2, date: 2, value: 25}]}
+        width={300}
+        height={200}
+        top={20}
+        bottom={30}
+        left={30}
+        right={0}
+      />
+      <Piechart
+        data={[{index: 0, value: 40},
+               {index: 1, value: 80},
+              ]}
+        width={200}
+        height={200}
+        innerRadius={60}
+        outerRadius={100}
+      />
     </div>
   );
 }
