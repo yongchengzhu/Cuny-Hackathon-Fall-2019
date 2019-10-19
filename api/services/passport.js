0d59efe124a6923158d6f718ab39dbbdbbb407d5
OrenBen-Meir <orenbm11@gmail.com>
2019-10-19 10:53:33 -0400

    refractored model import for password

--- api/services/passport.js
+++ api/services/passport.js
@@ -3,7 +3,7 @@ const JwtStrategy   = require('passport-jwt').Strategy;
 const ExtractJwt    = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
 
-const User = require('../models/user');
+const User = require('../models/').User;
 
 // -----------------------------------------------------------------------------------------
 // JWT Strategy
