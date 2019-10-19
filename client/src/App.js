d85a0c6bc37f0249d331592762bc4422c3a1b4e5
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2019-10-19 16:46:59 -0400

    fix with develop
    merging with develop

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Bargraph from './components/bargraph.js';
-import Piechart from './components/piechart';
-import SideBar from './components/sidebar-component';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
 import './css/sidebar.css';
-import aboutUsPage from './pages/about-us-page';
+import './css/App.css';
+
+
+
 import loginPage from './pages/login-page';
 import signUpPage from './pages/signup-page';
+import dashboard from './pages/dashboard-page';
+import groups from './pages/groups-page';
+import howto from './pages/howto-page';
+import aboutUsPage from './pages/about-us-page';
 
 function App() {
   return (
     <div>
-      <Bargraph
-        data={[{index: 0, date: 0, value: 15},
-               {index: 1, date: 1, value: 45}, 
-               {index: 2, date: 2, value: 25}]}
-        width={300}
-        height={200}
-        top={20}
-        bottom={30}
-        left={30}
-        right={0}
-      />
-      <Piechart
-        data={[{index: 0, value: 40},
-               {index: 1, value: 80},
-              ]}
-        width={200}
-        height={200}
-        innerRadius={60}
-        outerRadius={100}
-      />
-      
-      <SideBar />
 
       <Router>
           <Route path="/login" exact component={loginPage}/>
           <Route path="/signup" exact component={signUpPage}/>
-          <Route path="/dashboard" exact component={aboutUsPage}/>
-          {/* <Route path="/groups" exact component={}/> */}
-          {/* <Route path="/howto" exact component={}/> */}
+          <Route path="/dashboard" exact component={dashboard} /> 
+          <Route path="/groups" exact component={groups}/>
+          <Route path="/howto" exact component={howto}/>
+          <Route path="/about" exact component={aboutUsPage}/>
+
       </Router>
     </div>
   );
