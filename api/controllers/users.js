b2523f1e7e32bf5c1f44dea48c86c4c2bf7a691e
OrenBen-Meir <orenbm11@gmail.com>
2019-10-18 21:22:59 -0400

    setup controller template

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,7 +2,31 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to users api' });
+  // get info specific for user
+});
+
+router.post('/signin', (req, res) => {
+  // sign in user
+});
+
+router.post('/signup', (req, res) => {
+  // Sign up user
+});
+
+router.put('/update', (req, res) => {
+  // update user info
+});
+
+router.post('/carbonLog/create', (req, res) => {
+  // Create carbon log entry
+});
+
+router.delete('/update/carbonlog/:id', (req, res) => {
+  // delete carbon log entry
+});
+
+router.put('/update/carbonlog/:id', (req, res) => {
+  // update carbon log entry
 });
 
 module.exports = router;
