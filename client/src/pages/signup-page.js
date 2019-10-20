9e95729092881910946c674dc415585898b1eb8f
Rehman Arshad <rehman.arshad777@gmail.com>
2019-10-19 21:19:36 -0400

    fixed scroll bug 2, and color animation scalable

--- client/src/pages/signup-page.js
+++ client/src/pages/signup-page.js
@@ -41,62 +41,64 @@ export default class CreateUser extends Component {
   render() {
     return (
       <div className="row no-gutter ">
-           
-        <div className="col-md-8 col-lg-6">
-          <div className="login d-flex align-items-center py-5">
-            <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-9 col-lg-8 mx-auto">
-              
-              <img className="sign-Up-Logo align-items-center text-Label" src={logo} width="250px" height="250px" alt="Climater Logo"/>
+        <div className="wrapper">
+          <div className="col">
+            <div className="login d-flex align-items-center py-5">
+              <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-9 col-lg-8 mx-auto">
                 
-                <h3 className="login-heading mb-4 align-items-center text-Label">Create an account!</h3>
-                <form id="signupform">
-                  <label htmlFor="inputName" className="text-center" >User Name</label>
-                  <div className="form-label-group">
-                    <input name="username" type="username" id="inputUsername" className="form-control" required autoFocus/>
-                  </div>
+                <img className="sign-Up-Logo align-items-center text-Label" src={logo} width="250px" height="250px" alt="Climater Logo"/>
+                  
+                  <h3 className="login-heading mb-4 align-items-center text-Label">Create an account!</h3>
+                  <form id="signupform">
+                    <label htmlFor="inputName" className="text-center" >User Name</label>
+                    <div className="form-label-group">
+                      <input name="username" type="username" id="inputUsername" className="form-control" required autoFocus/>
+                    </div>
 
-                  <label htmlFor="inputEmail"> Email address</label>
-                  <div className="form-label-group">
-                    <input name="email" type="email" id="inputEmail" className="form-control" required autoFocus/>
-                  </div>
+                    <label htmlFor="inputEmail"> Email address</label>
+                    <div className="form-label-group">
+                      <input name="email" type="email" id="inputEmail" className="form-control" required autoFocus/>
+                    </div>
 
-                  <label htmlFor="inputPassword">Password</label>
-                  <div className="form-label-group">
-                    <input name="password" type="password" id="inputPassword" className="form-control" required/> 
-                  </div>
+                    <label htmlFor="inputPassword">Password</label>
+                    <div className="form-label-group">
+                      <input name="password" type="password" id="inputPassword" className="form-control" required/> 
+                    </div>
 
-                  <label htmlFor="confirmPassword">Confirm Password</label>
-                  <div className="form-label-group"> 
-                    <input type="password" id="ConfirmPassword" className="form-control" required/> 
-                  </div>
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <div className="form-label-group"> 
+                      <input type="password" id="ConfirmPassword" className="form-control" required/> 
+                    </div>
 
-                  <label htmlFor="inputPhoneNumber">Phone number</label>
-                  <div className="form-label-group">
-                    <input name="mobile" type="Number" id="inputMobile" className="form-control" required/> 
-                  </div>
+                    <label htmlFor="inputPhoneNumber">Phone number</label>
+                    <div className="form-label-group">
+                      <input name="mobile" type="Number" id="inputMobile" className="form-control" required/> 
+                    </div>
 
-                  <label htmlFor="inputPhoneNumber">Fuel Economy</label>
-                  <div className="form-label-group">
-                    <input name="milesPerGallon" type="Number" id="inputFuelEconomy" className="form-control" required/> 
-                  </div>
+                    <label htmlFor="inputPhoneNumber">Fuel Economy</label>
+                    <div className="form-label-group">
+                      <input name="milesPerGallon" type="Number" id="inputFuelEconomy" className="form-control" required/> 
+                    </div>
 
-                  <button 
-                    className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" 
-                    type="submit"
-                    onClick = {e => this.onSubmit(e)}
-                  >
-                    Register
-                  </button>
-                  <a className="d-block text-center mt-3 medium" href="/">Already have an account?</a>
-                </form>
+                    <button 
+                      className="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" 
+                      type="submit"
+                      onClick = {e => this.onSubmit(e)}
+                    >
+                      Register
+                    </button>
+                    <a className="d-block text-center mt-3 medium" href="/">Already have an account?</a>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-
+      
+      
       <div className="d-none d-md-flex col-md-4 col-lg-6 ">
         <section>
         </section>
